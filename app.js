889172e39de357b2035949a2a791f0de236bcdba
LJMas <lucy.massie@madetech.com>
2022-07-27 15:40:40 +0100

    End of Bobby's demo

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var nunjucks = require('nunjucks');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -10,14 +11,24 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+
+nunjucks.configure([
+  "node_modules/govuk-frontend/",
+  "views"
+], {
+  autoescape: true,
+  express: app
+});
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/assets', express.static(path.join(__dirname, '/node_modules/govuk-frontend/govuk/assets')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index.html');
+});
+
+/* GET html page. */
 router.get('/html', function(req, res, next) {
   // res.render('index', { title: 'Express' });
-  res.redirect('html-view.html');
+  res.render('html-view.html');
+});
+
+/* GET nunjuck page */
+router.get('/nunjuck', function(req, res, next) {
+  // res.render('index', { title: 'Express' });
+  res.render('nunjuck-view.html');
 });
 
 module.exports = router;
